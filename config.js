dcf31c3ab40e702faf86403f8006cfe90f211645
abeld12 <126180102+abeld12@users.noreply.github.com>
2024-02-09 20:53:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['237680962134', 'GURU', true],
+  ['694049028', 'Ash', true], 
   [''] 
 ] //Number of owners
 
